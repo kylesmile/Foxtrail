d11b063a9c7b32f99fc9898e3e9df6459d678053
Kyle Smith <kyle.smith@rolemodelsoftware.com>
2017-07-15 10:17:50 -0500

    Create a test scene and add Ben's assets

---
+++ Assets/FoxController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxController : MonoBehaviour {
+	public float movementSpeed = 1.0f;
+
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis ("Horizontal") * Time.deltaTime * movementSpeed;
+		float vertical = Input.GetAxis ("Vertical") * Time.deltaTime * movementSpeed;
+
+		transform.Translate (horizontal, vertical, 0);
+	}
+}
