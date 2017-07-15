5665538a68bd8ef286042a73f9f0d78ba9410c9c
Kyle Smith <kyle.smith@rolemodelsoftware.com>
2017-07-15 12:08:58 -0500

    Make the fox drop scents

--- Assets/FoxController.cs
+++ Assets/FoxController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class FoxController : MonoBehaviour {
 	public float movementSpeed = 1.0f;
+	public float scentDistance = 3.0f;
+	public Scent scentPrefab;
+
+	private float accumulatedDistance = 0.0f;
 
 	// Update is called once per frame
 	void Update () {
@@ -11,11 +15,17 @@ public class FoxController : MonoBehaviour {
 		float vertical = Input.GetAxisRaw ("Vertical") * Time.deltaTime * movementSpeed;
 		Vector3 direction = new Vector3 (horizontal, vertical, 0);
 
-
 		if (direction != Vector3.zero) {
 			float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 			transform.Translate (transform.InverseTransformDirection(direction));
+		}
+
+		accumulatedDistance += direction.magnitude;
+
+		if (accumulatedDistance > scentDistance) {
+			accumulatedDistance = 0.0f;
+			Instantiate (scentPrefab, transform.position, transform.rotation);
 		}
 	}
 }
