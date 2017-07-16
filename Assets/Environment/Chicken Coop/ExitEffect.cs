0304d17e27991901e069e1d7de8e9c2cafb4b775
Kyle Smith <kyle.smith@rolemodelsoftware.com>
2017-07-16 17:07:31 -0500

    Re-add arrow and add dying chicken sound effect

--- Assets/Environment/Chicken Coop/ExitEffect.cs
+++ Assets/Environment/Chicken Coop/ExitEffect.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class ExitEffect : MonoBehaviour {
 
 	private ParticleSystem featherParticles;
+	private AudioSource dyingChicken;
 
 	void Start () {
 		featherParticles = GetComponent<ParticleSystem> ();
+		dyingChicken = GetComponent<AudioSource> ();
 	}
 
 	public void Play () {
 		featherParticles.Play ();
+		dyingChicken.Play ();
 	}
 }
